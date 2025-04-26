13889475aa558c52418fe754e06fc34f1cca3a17
luo_-cc <cco.oul@gmail.com>
2025-04-26 22:44:25 +0800

    add archive

--- eudic/dist/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const api_js_1 = require("./api.js");
-(0, api_js_1.addStudyListCategory)('en', 'qwerty')
-    .then((result) => console.log('Category added:', result))
-    .catch((error) => console.error('Error adding category:', error));
--- eudic/test.ts
+++
@@ -1,7 +0,0 @@
-namespace EudicTest {
-  const { deleteStudyListCategory } = require('./api')
-
-  deleteStudyListCategory('133900804293146580', 'en', 'qwerty')
-    .then((result) => console.log('Category deleted:', result))
-    .catch((error) => console.error('Error deleting category:', error))
-}
